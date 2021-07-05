63bf773ad255991f86ff76d42b7e445d71f8af4d
adam <everett.ingram22@gmail.com>
2021-07-05 16:20:57 -0400

    Monolith created

---
+++ Demo Course Monolithic Application/CourseMonolithic/src/main/java/com/example/coursemonolithic/CourseMonolithicApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursemonolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMonolithicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseMonolithicApplication.class, args);
+    }
+
+}
---
+++ Demo Course Monolithic Application/CourseMonolithic/src/test/java/com/example/coursemonolithic/CourseMonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursemonolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMonolithicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
