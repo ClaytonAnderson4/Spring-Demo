f675b1ca26506f1dd8857fc718b6e2b5843dae6a
adam <everett.ingram22@gmail.com>
2021-07-06 12:08:57 -0400

    Added URL notes

--- SpringHelloWorld/src/main/java/com/spring/microservices/controller/HelloService.java
+++ SpringHelloWorld/src/main/java/com/spring/microservices/controller/HelloService.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloService {
 
+    //http://localhost:8080/api/v1/
     @GetMapping("/")
     public String index() {
         return "Hello World 2021...";
